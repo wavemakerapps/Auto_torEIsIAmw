f86aaf4f6d849681347a715a9f60ffb9a3982628
root <root@d3a2e0704417>
2017-06-06 06:23:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_torEIsIAmw",
+  "homePage" : "Main",
+  "name" : "Auto_torEIsIAmw",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
